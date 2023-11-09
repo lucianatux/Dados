2787a1dd592b0a2df7393ac51548309089b4e867
lucianatux <lucianatux@yahoo.com.ar>
2023-11-08 21:39:39 -0300

    hero quest dices

--- script.js
+++ script.js
@@ -21,6 +21,54 @@ document.addEventListener("DOMContentLoaded", function () {
         multiDiceResult.innerHTML = "Por favor, ingresa una cantidad válida de dados (1-6).";
       }
     });
+
+    const specialDiceButton = document.getElementById("specialDiceButton");
+  const specialDiceImage = document.getElementById("specialDiceImage");
+  const multiSpecialDiceButton = document.getElementById("multiSpecialDiceButton");
+  const multiSpecialDiceResult = document.getElementById("multiSpecialDiceResult");
+
+  specialDiceButton.addEventListener("click", function () {
+    rollSpecialDice();
+  });
+
+  multiSpecialDiceButton.addEventListener("click", function () {
+    const numberOfSpecialDice = parseInt(numberOfDiceInput.value, 10);
+    if (numberOfSpecialDice > 0 && numberOfSpecialDice <= 6) {
+      multiSpecialDiceResult.innerHTML = ""; // Limpia los resultados anteriores
+      for (let i = 0; i < numberOfSpecialDice; i++) {
+        rollSpecialDice();
+      }
+    } else {
+      multiSpecialDiceResult.innerHTML = "Por favor, ingresa una cantidad válida de dados especiales (1-6).";
+    }
+  });
+
+  function rollSpecialDice() {
+    const specialRandomNumber = getRandomNumber(1, 6);
+    let specialImageName = "";
+
+    switch (specialRandomNumber) {
+      case 1:
+        specialImageName = "dicemonster.jpg";
+        break;
+      case 2:
+      case 3:
+        specialImageName = "dicelion.jpg";
+        break;
+      case 4:
+      case 5:
+      case 6:
+        specialImageName = "diceskull.jpg";
+        break;
+      default:
+        break;
+    }
+
+    const specialDiceImageElement = document.createElement("img");
+    specialDiceImageElement.src = specialImageName;
+    specialDiceImageElement.alt = "Dado Especial";
+    multiSpecialDiceResult.appendChild(specialDiceImageElement);
+  }
   
     function getRandomNumber(min, max) {
       return Math.floor(Math.random() * (max - min + 1)) + min;
